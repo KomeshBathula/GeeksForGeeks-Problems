5ac9bf2a8936e12a88576660f0e913b3cbab1179
Komesh Bathula <komeshbathula@gmail.com>
2025-09-21 20:22:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print first letter of every word in the string/print-first-letter-of-every-word-in-the-string.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+
+class Solution {
+    String firstAlphabet(String S) {
+        // code here
+        StringBuilder sb = new StringBuilder();
+        sb.append(S.charAt(0));
+        for(int i = 1;i<S.length();i++){
+            if(S.charAt(i) == ' ') sb.append(S.charAt(i+1));
+        }
+        return sb.toString();
+    }
+};
