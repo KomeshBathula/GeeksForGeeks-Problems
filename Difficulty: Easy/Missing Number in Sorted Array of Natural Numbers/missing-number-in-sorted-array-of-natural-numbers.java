92b4de0c36a0bc7e4e472678918e887c78f6a381
Komesh Bathula <komeshbathula@gmail.com>
2025-11-23 18:26:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing Number in Sorted Array of Natural Numbers/missing-number-in-sorted-array-of-natural-numbers.java
@@ -0,0 +1,12 @@
+class Solution {
+    int missingNumber(int arr[]) {
+        // code here
+        //using O(n)
+        //extreme brute force approach
+        
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i] != i+1) return i+1;
+        }
+        return arr.length + 1;
+    }
+}
