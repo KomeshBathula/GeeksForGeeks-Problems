41fa76d8bd7c2d96b54dc0c91e265535f91c283b
Komesh Bathula <komeshbathula@gmail.com>
2025-12-18 19:29:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort in specific order/sort-in-specific-order.java
+++ Difficulty: Medium/Sort in specific order/sort-in-specific-order.java
@@ -1,38 +1,69 @@
 class Solution {
     public void sortIt(int[] arr) {
         // code here
-        int left = 0, right = arr.length - 1;
-        while(left<right){
-            if(arr[left]%2 == 0 && arr[right]%2 != 0){
+        //brute force idea 
+        ArrayList<Integer> oddNumbers = new ArrayList<>();
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
+        for (int val : arr) {
+            if(val % 2 != 0) oddNumbers.add(val);
+            else evenNumbers.add(val);
+        }
+        int idx = 0;
+        for (int x : oddNumbers) arr[idx ++] = x;
+        for (int x : evenNumbers) arr[idx ++] = x;
+        
+        int low = 0, high = arr.length - 1;
+        quickSort1(arr, low, oddNumbers.size() - 1);
+        quickSort2(arr, oddNumbers.size(), high);
+    }
+    public void quickSort1(int arr[], int low, int high) {
+        if ( low >= high) return ;
+        int partitionIndex = partition1(arr, low, high);
+        quickSort1(arr, low, partitionIndex - 1);
+        quickSort1(arr, partitionIndex + 1, high);
+    }
+    public void quickSort2(int arr[], int low, int high) {
+        if ( low >= high) return ;
+        int partitionIndex = partition2(arr, low, high);
+        quickSort2(arr, low, partitionIndex - 1);
+        quickSort2(arr, partitionIndex + 1, high);
+    }
+    public int partition1(int arr[], int low, int high) {
+        int pivot = arr[low];
+        int left = low + 1, right = high;
+        while (left <= right) {
+            while( left <= high && arr[left] >= pivot)
+                left ++;
+            while (arr[right] < pivot && right >= low + 1) 
+                right --;
+            if (left < right) {
                 int temp = arr[left];
                 arr[left] = arr[right];
                 arr[right] = temp;
-                left ++;
-                right --;
             }
-            else if(arr[left]%2 != 0) left ++;
-            else right--;
         }
-        int breakPoint = arr.length;
-        for(int i = 0;i<arr.length;i++){
-            if(arr[i]%2 == 0) {
-                breakPoint = i;
-                break;
-            }  
-        }
-        //sort the odds and reverse the array
-        Arrays.sort(arr, 0, breakPoint);
-        reverseArray(arr, 0, breakPoint - 1);
-        //sort the even array
-        Arrays.sort(arr, breakPoint, arr.length);
+        int temp = arr[low];
+        arr[low] = arr[right];
+        arr[right] = temp;
+        return right;
     }
-    static void reverseArray(int arr[], int start, int end){
-        while(start<end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+    public int partition2(int arr[], int low, int high) {
+        int pivot = arr[low];
+        int left = low + 1, right = high;
+        while (left <= right) {
+            while(left <= high && arr[left] <= pivot)
+                left ++;
+            while (arr[right] > pivot && right >= low + 1) 
+                right --;
+            if (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
         }
+        int temp = arr[low];
+        arr[low] = arr[right];
+        arr[right] = temp;
+        return right;
     }
 }
